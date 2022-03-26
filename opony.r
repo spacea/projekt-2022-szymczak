031cc8bed3defbf0fa7f56a6949b89dd5d00a626
Kornel Szymczak <kornelm00@wp.pl>
2022-03-26 18:21:11 +0100

    dodalem ramkę z oponami

---
+++ opony.r
@@ -0,0 +1,4 @@
+#Opony
+Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Twarda", "Pośrednia", "Miękka"),
+                                     Lifetime = c(200, 150 ,100))
+Dostepne_mieszanki_opon
