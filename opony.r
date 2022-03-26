5c9c42c4d30de3769503e792dde6b36dce37cfaf
Wys21 <janwysocki4102002@wp.pl>
2022-03-26 18:25:07 +0100

    zmieniłem nazwy opon na język angielski

--- opony.r
+++ opony.r
@@ -1,4 +1,4 @@
 #Opony
-Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Twarda", "Pośrednia", "Miękka"),
+Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
                                      Lifetime = c(200, 150 ,100))
 Dostepne_mieszanki_opon
