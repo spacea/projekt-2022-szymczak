f2999ef4ad66f07359f89c255e53a8c0f71c49e9
Wys21 <janwysocki4102002@wp.pl>
2022-03-29 19:06:27 +0200

    poprawiłem

--- Monako.R
+++ Monako.R
@@ -1,5 +1,5 @@
 #Monako
 install.packages("readxl")
 library(readxl)
-Circuit_de_Monaco <- read_excel("C:/Users/Jan/Desktop/Circuit de Monaco.xlsx")
+Circuit_de_Monaco <- read_excel("Circuit de Monaco.xlsx")
 View(Circuit_de_Monaco)
