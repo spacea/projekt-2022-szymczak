228d9009b6384efcd2c7e249bc08ecd4a3e9ff5d
imie nazwisko <kornelm00@wp.pl>
2022-03-30 17:46:55 +0200

    poprawiłem kod

--- Spa_Francorchamps.R
+++ Spa_Francorchamps.R
@@ -1,5 +1,5 @@
 #Spa
 install.packages("readxl")
 library(readxl)
-Spa_Francorchamps = read_excel("C:/Users/Kornel/Desktop/F1_project/Spa.xlsx")
+Spa_Francorchamps = read_excel("Spa.xlsx")
 Spa_Francorchamps
