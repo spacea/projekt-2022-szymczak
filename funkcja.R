7cf74d99f8a63b247b8a451c57e4e0eb08e2d06c
imie nazwisko <kornelm00@wp.pl>
2022-03-30 19:42:35 +0200

    poprawiłem kod- nawiasy

--- funkcja.R
+++ funkcja.R
@@ -9,7 +9,7 @@ taktyka_opony = function(tor){
       stop("Podaj jeden z trzech torów wymienionych w instrukcji w README")
     }
   if(tor == "Spa"){
-    czas_przejazdu_na_softach_spa = (Dostepne_mieszanki_opon[3 , 2] / (sample(c(Spa_Francorchamps[6 , 2] : Spa_Francorchamps[7 , 2]), 1) * Spa_Francorchamps[4 , 2])) * Dostepne_mieszanki_opon[3 , 3]
+    czas_przejazdu_na_softach_spa = Dostepne_mieszanki_opon[3 , 2] / ((sample(c(Spa_Francorchamps[6 , 2] : Spa_Francorchamps[7 , 2]), 1) * Spa_Francorchamps[4 , 2]) * Dostepne_mieszanki_opon[3 , 3])
     okrazenie = Spa_Francorchamps[4 , 2] * czas_przejazdu_na_softach_spa
     round(okrazenie, 0)
     czas_przejazdu_na_medach_spa = Dostepne_mieszanki_opon[2 , 2] / ((sample(c(Spa_Francorchamps[6 , 2] : Spa_Francorchamps[7 , 2]), 1) * Spa_Francorchamps[4 , 2]) * Dostepne_mieszanki_opon[2 , 3])
@@ -20,7 +20,7 @@ taktyka_opony = function(tor){
     round(okrazenie, 0) 
   }
   if(tor == "Monako"){
-    czas_przejazdu_na_softach_monako = (Dostepne_mieszanki_opon[3 , 2] / (sample(c(Circuit_de_Monaco[6 , 2] : Circuit_de_Monaco[7 , 2]), 1) * Circuit_de_Monaco[4 , 2])) * Dostepne_mieszanki_opon[3 , 3]
+    czas_przejazdu_na_softach_monako = Dostepne_mieszanki_opon[3 , 2] / ((sample(c(Circuit_de_Monaco[6 , 2] : Circuit_de_Monaco[7 , 2]), 1) * Circuit_de_Monaco[4 , 2]) * Dostepne_mieszanki_opon[3 , 3])
     okrazenie = Monako[4 , 2] * czas_przejazdu_na_softach_monako
     round(okrazenie, 0)
     czas_przejazdu_na_medach_monako = Dostepne_mieszanki_opon[2 , 2] / ((sample(c(Circuit_de_Monaco[6 , 2] : Circuit_de_Monaco[7 , 2]), 1) * Circuit_de_Monaco[4 , 2]) * Dostepne_mieszanki_opon[2 , 3])
@@ -32,7 +32,7 @@ taktyka_opony = function(tor){
   }
   if(tor == "Monza")
   {
-    czas_przejazdu_na_softach_monza = (Dostepne_mieszanki_opon[3 , 2] / (sample(c(Autodromo_Natzionale_Monza[6 , 2] : Autodromo_Natzionale_Monza[7 , 2]), 1) * Autodromo_Natzionale_Monza[4 , 2])) * Dostepne_mieszanki_opon[3 , 3]
+    czas_przejazdu_na_softach_monza = Dostepne_mieszanki_opon[3 , 2] / ((sample(c(Autodromo_Natzionale_Monza[6 , 2] : Autodromo_Natzionale_Monza[7 , 2]), 1) * Autodromo_Natzionale_Monza[4 , 2]) * Dostepne_mieszanki_opon[3 , 3])
     okrazenie = Monza [4 , 2] * czas_przejazdu_na_softach_monza
     round(okrazenie, 0)
     czas_przejazdu_na_medach_monza = Dostepne_mieszanki_opon[2 , 2] / ((sample(c(Autodromo_Natzionale_Monza[6 , 2] : Autodromo_Natzionale_Monza[7 , 2]), 1) * Autodromo_Natzionale_Monza[4 , 2]) * Dostepne_mieszanki_opon[2 , 3])
