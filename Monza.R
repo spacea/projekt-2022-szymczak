cb04352345367922e48154c42e1ec67cbade18e6
czaja19.03.2002@gmail.com <czaja19.03.2002@gmail.com>
2022-03-30 18:26:43 +0200

    Ramka danych z Monzy

--- Monza.R
+++ Monza.R
@@ -1,3 +1,5 @@
 library(readxl)
 Autodromo_Natzionale_Monza <- read_excel("Autodromo_Natzionale_Monza.xlsx")
 View(Autodromo_Natzionale_Monza)
+Autodromo_Natzionale_Monza = as.data.frame(Autodromo_Natzionale_Monza)
+Autodromo_Natzionale_Monza
