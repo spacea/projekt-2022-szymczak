cde7c611aa38aa9ee19b04386007f09cdef54aaa
imie nazwisko <kornelm00@wp.pl>
2022-03-30 18:40:45 +0200

    dodałem współczynniki szybkości opon

--- opony.r
+++ opony.r
@@ -1,4 +1,5 @@
 #Opony
 Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
-                                     Lifetime = c(4500, 3500 ,2500))
+                                     Lifetime = c(4500, 3500 ,2500),
+                                     Speed = c(2.2, 1.6, 1))
 Dostepne_mieszanki_opon
