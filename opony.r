b9ca8ca20cc141b0467d524c395327fe87983495
imie nazwisko <kornelm00@wp.pl>
2022-03-30 22:24:49 +0200

    kolejna zmiana w lifetimie

--- opony.r
+++ opony.r
@@ -1,5 +1,5 @@
 #Opony
 Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
-                                     Lifetime = c(2800, 2000 ,1300),
+                                     Lifetime = c(3100, 2200 ,1100),
                                      Speed = c(1.02, 1.015, 1))
 Dostepne_mieszanki_opon
