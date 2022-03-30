1a961e8051cd5d4f3c7a96e4f10e1a8d33a0d2a9
imie nazwisko <kornelm00@wp.pl>
2022-03-30 22:45:21 +0200

    poprawiony speed hardów

--- opony.r
+++ opony.r
@@ -1,5 +1,5 @@
 #Opony
 Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
                                      Lifetime = c(3100, 2200 ,1100),
-                                     Speed = c(1.02, 1.015, 1))
+                                     Speed = c(1.07, 1.015, 1))
 Dostepne_mieszanki_opon
