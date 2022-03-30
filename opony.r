926ce97eb17e0513467fbc6078a12ad889018baa
imie nazwisko <kornelm00@wp.pl>
2022-03-30 19:03:09 +0200

    poprawiłem parametr speed

--- opony.r
+++ opony.r
@@ -1,5 +1,5 @@
 #Opony
 Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
-                                     Lifetime = c(4500, 3500 ,2500),
-                                     Speed = c(2.2, 1.6, 1))
+                                     Lifetime = c(2000, 1300 ,800),
+                                     Speed = c(1.02, 1.015, 1))
 Dostepne_mieszanki_opon
