ee786d3bf201fe0146ffb71ddbff55a8ba8ef5cf
Wys21 <janwysocki4102002@wp.pl>
2022-03-31 00:21:06 +0200

    dodałem funkcję do toru Monza

---
+++ funkcja_Monza.R
@@ -0,0 +1,29 @@
+#funkcja_Monza
+taktyka_opony_monza = function(Monza){
+  race_time = sample(c(Autodromo_Natzionale_Monza[6 , 2] : Autodromo_Natzionale_Monza[7 , 2]), 1) * Autodromo_Natzionale_Monza[4 , 2]
+  czas_przejazdu_na_softach_monza = Dostepne_mieszanki_opon[3 , 2] / (race_time * Dostepne_mieszanki_opon[3 , 3])
+  okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_softach_monza
+  monza_softy = round(okrazenia, 0)
+  czas_przejazdu_na_medach_monza = Dostepne_mieszanki_opon[2 , 2] / (race_time * Dostepne_mieszanki_opon[2 , 3])
+  okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_medach_monza
+  monza_medy = round(okrazenia, 0)
+  czas_przejazdu_na_hardach_monza = Dostepne_mieszanki_opon[1 , 2] / (race_time * Dostepne_mieszanki_opon[1 , 3])
+  okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_hardach_monza
+  monza_hardy = round(okrazenia, 0)
+  konfiguracje_opon = c((2 * Autodromo_Natzionale_Monza[8 , 2]) + ((2 * monza_softy) * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])), (2 * Autodromo_Natzionale_Monza[8 , 2]) + (monza_softy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_medy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])), Autodromo_Natzionale_Monza[8 , 2] + ((2 * monza_hardy) * (race_time / Autodromo_Natzionale_Monza[4 , 2])), (Autodromo_Natzionale_Monza[8 , 2]) + (monza_medy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])))
+  taktyka = which.min(abs(konfiguracje_opon - race_time))
+  taktyka
+  if(taktyka == 1){
+    cat("Na ten wyścig najlepiej będzie wybrać dwa komplety miękkich opon i komplet twardych")
+  }
+  if(taktyka == 2){
+    cat("Na ten wyścig najlepiej będzie wybrać po jednym komplecie opon miękkich, średnich i twardych")
+  }
+  if(taktyka == 3){
+    cat("Na ten wyścig najlepiej będzie wybrać dwa komplety opon twardych")
+  }
+  if(taktyka == 4){
+    cat("Na ten wyścig najlepiej będzie wybrać po jednym komplecie opon średnich i twardych")
+  }
+}
+taktyka_opony_monza(monza)
