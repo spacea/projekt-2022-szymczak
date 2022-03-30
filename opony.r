9366a5bc6a5398eda8d81b3e059c0d382aa96a20
imie nazwisko <kornelm00@wp.pl>
2022-03-30 17:20:28 +0200

    poprawiłem lifetime opon

--- opony.r
+++ opony.r
@@ -1,4 +1,4 @@
 #Opony
 Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
-                                     Lifetime = c(200, 150 ,100))
+                                     Lifetime = c(4500, 3500 ,2500))
 Dostepne_mieszanki_opon
