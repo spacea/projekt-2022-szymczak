a45726d6ff417e9fae07c01a280090cec012f246
Wys21 <janwysocki4102002@wp.pl>
2022-04-06 16:35:51 +0200

    poprawiłem w funkcjach

--- scripts/funkcja_Monako.R
+++ scripts/funkcja_Monako.R
@@ -1,18 +1,36 @@
-#funkcja_Monako
+#pakiet
+library(readxl)
+
+#funkcja Monako
 taktyka_opony_monako = function(Monako){
-  race_time = sample(c(Circuit_de_Monaco[6 , 2] : Circuit_de_Monaco[7 , 2]), 1) * Circuit_de_Monaco[4 , 2]
+
+#dane dotyczące toru Monako
+Circuit_de_Monaco = read_excel("excel/Circuit de Monaco.xlsx")
+Circuit_de_Monaco = as.data.frame(Circuit_de_Monaco)
+
+#parametry opon
+  Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
+                                       Lifetime = c(3100, 2200 ,1100),
+                                       Speed = c(1.07, 1.015, 1))    
+
+#czas wyscigu(bedzie rozny dla kazdej z mieszanek i  losowy)
+race_time = sample(c(Circuit_de_Monaco[6 , 2] : Circuit_de_Monaco[7 , 2]), 1) * Circuit_de_Monaco[4 , 2]
   czas_przejazdu_na_softach_monako = Dostepne_mieszanki_opon[3 , 2] / (race_time * Dostepne_mieszanki_opon[3 , 3])
-  okrazenia = Circuit_de_Monaco[4 , 2] * czas_przejazdu_na_softach_monako
-  monako_softy = round(okrazenia, 0)
+    okrazenia = Circuit_de_Monaco[4 , 2] * czas_przejazdu_na_softach_monako
+      monako_softy = round(okrazenia, 0)
   czas_przejazdu_na_medach_monako = Dostepne_mieszanki_opon[2 , 2] / (race_time * Dostepne_mieszanki_opon[2 , 3])
-  okrazenia = Circuit_de_Monaco[4 , 2] * czas_przejazdu_na_medach_monako
-  monako_medy = round(okrazenia, 0)
+    okrazenia = Circuit_de_Monaco[4 , 2] * czas_przejazdu_na_medach_monako
+      monako_medy = round(okrazenia, 0)
   czas_przejazdu_na_hardach_monako = Dostepne_mieszanki_opon[1 , 2] / (race_time * Dostepne_mieszanki_opon[1 , 3])
-  okrazenia = Circuit_de_Monaco[4 , 2] * czas_przejazdu_na_hardach_monako
-  monako_hardy = round(okrazenia, 0)
-  konfiguracje_opon = c((2 * Circuit_de_Monaco[8 , 2]) + ((2 * monako_softy) * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_hardy * (race_time / Circuit_de_Monaco[4 , 2])), (2 * Circuit_de_Monaco[8 , 2]) + (monako_softy * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_medy * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_hardy * (race_time / Circuit_de_Monaco[4 , 2])), Circuit_de_Monaco[8 , 2] + ((2 * monako_hardy) * (race_time / Circuit_de_Monaco[4 , 2])), (Circuit_de_Monaco[8 , 2]) + (monako_medy * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_hardy * (race_time / Circuit_de_Monaco[4 , 2])))
-  taktyka = which.min(abs(konfiguracje_opon - race_time))
-  taktyka
+    okrazenia = Circuit_de_Monaco[4 , 2] * czas_przejazdu_na_hardach_monako
+      monako_hardy = round(okrazenia, 0)
+
+#dostepne konfiguracje opon      
+konfiguracje_opon = c((2 * Circuit_de_Monaco[8 , 2]) + ((2 * monako_softy) * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_hardy * (race_time / Circuit_de_Monaco[4 , 2])), (2 * Circuit_de_Monaco[8 , 2]) + (monako_softy * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_medy * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_hardy * (race_time / Circuit_de_Monaco[4 , 2])), Circuit_de_Monaco[8 , 2] + ((2 * monako_hardy) * (race_time / Circuit_de_Monaco[4 , 2])), (Circuit_de_Monaco[8 , 2]) + (monako_medy * (race_time / Circuit_de_Monaco[4 , 2])) + (monako_hardy * (race_time / Circuit_de_Monaco[4 , 2])))
+  
+#taktyka  
+taktyka = which.min(abs(konfiguracje_opon - race_time))
+
   if(taktyka == 1){
     cat("Na ten wyścig najlepiej będzie wybrać dwa komplety miękkich opon i komplet twardych")
   }
@@ -26,5 +44,4 @@ taktyka_opony_monako = function(Monako){
     cat("Na ten wyścig najlepiej będzie wybrać po jednym komplecie opon średnich i twardych")
   }
 }
-
 taktyka_opony_monako(monako)
--- scripts/funkcja_Monza.R
+++ scripts/funkcja_Monza.R
@@ -1,19 +1,36 @@
-#funkcja_Monza
+#pakiet
+library(readxl)
 
+#funkcja Monza
 taktyka_opony_monza = function(Monza){
-  race_time = sample(c(Autodromo_Natzionale_Monza[6 , 2] : Autodromo_Natzionale_Monza[7 , 2]), 1) * Autodromo_Natzionale_Monza[4 , 2]
+  
+#dane dotyczace toru Monza
+Autodromo_Natzionale_Monza = read_excel("excel/Autodromo_Natzionale_Monza.xlsx")
+Autodromo_Natzionale_Monza = as.data.frame(Autodromo_Natzionale_Monza)
+  
+#parametry opon  
+Dostepne_mieszanki_opon = data.frame(Mieszanki = c("Hard", "Medium", "Soft"),
+                                     Lifetime = c(3100, 2200 ,1100),
+                                     Speed = c(1.07, 1.015, 1))
+
+#czas wyscigu(bedzie rozny dla kazdej z mieszanek i losowy)
+race_time = sample(c(Autodromo_Natzionale_Monza[6 , 2] : Autodromo_Natzionale_Monza[7 , 2]), 1) * Autodromo_Natzionale_Monza[4 , 2]
   czas_przejazdu_na_softach_monza = Dostepne_mieszanki_opon[3 , 2] / (race_time * Dostepne_mieszanki_opon[3 , 3])
-  okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_softach_monza
-  monza_softy = round(okrazenia, 0)
+    okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_softach_monza
+      monza_softy = round(okrazenia, 0)
   czas_przejazdu_na_medach_monza = Dostepne_mieszanki_opon[2 , 2] / (race_time * Dostepne_mieszanki_opon[2 , 3])
-  okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_medach_monza
-  monza_medy = round(okrazenia, 0)
+    okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_medach_monza
+      monza_medy = round(okrazenia, 0)
   czas_przejazdu_na_hardach_monza = Dostepne_mieszanki_opon[1 , 2] / (race_time * Dostepne_mieszanki_opon[1 , 3])
-  okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_hardach_monza
-  monza_hardy = round(okrazenia, 0)
-  konfiguracje_opon = c((2 * Autodromo_Natzionale_Monza[8 , 2]) + ((2 * monza_softy) * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])), (2 * Autodromo_Natzionale_Monza[8 , 2]) + (monza_softy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_medy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])), Autodromo_Natzionale_Monza[8 , 2] + ((2 * monza_hardy) * (race_time / Autodromo_Natzionale_Monza[4 , 2])), (Autodromo_Natzionale_Monza[8 , 2]) + (monza_medy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])))
-  taktyka = which.min(abs(konfiguracje_opon - race_time))
-  taktyka
+    okrazenia = Autodromo_Natzionale_Monza[4 , 2] * czas_przejazdu_na_hardach_monza
+      monza_hardy = round(okrazenia, 0)
+
+#dostepne konfiguracje opon  
+konfiguracje_opon = c((2 * Autodromo_Natzionale_Monza[8 , 2]) + ((2 * monza_softy) * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])), (2 * Autodromo_Natzionale_Monza[8 , 2]) + (monza_softy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_medy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])), Autodromo_Natzionale_Monza[8 , 2] + ((2 * monza_hardy) * (race_time / Autodromo_Natzionale_Monza[4 , 2])), (Autodromo_Natzionale_Monza[8 , 2]) + (monza_medy * (race_time / Autodromo_Natzionale_Monza[4 , 2])) + (monza_hardy * (race_time / Autodromo_Natzionale_Monza[4 , 2])))
+
+#taktyka 
+taktyka = which.min(abs(konfiguracje_opon - race_time))
+  
   if(taktyka == 1){
     cat("Na ten wyścig najlepiej będzie wybrać dwa komplety miękkich opon i komplet twardych")
   }
